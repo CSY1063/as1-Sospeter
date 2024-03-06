53772836e8afe21036c72c06e0d4d0a50ce6f027
Sospeter Kedogo <102984439+SospeterKe@users.noreply.github.com>
2024-03-06 20:22:25 +0000

    fixed background resizing on nav toggle

--- javascript.js
+++ javascript.js
@@ -2,10 +2,10 @@
 
 function closeNav() {
     nav = document.getElementById("nav");
-    nav.style.left = "100%";
+    nav.style.left = "-50em";
 }
 
 function openNav() {
     nav = document.getElementById("nav");
-    nav.style.left = "0%";
+    nav.style.left = "0em";
 }
