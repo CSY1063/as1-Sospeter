1cf165840e797572a418cc8f82cbe438996cba33
Sospeter <kedogosospeter36@gmail.com>
2024-03-06 14:58:52 +0000

    added hamburger icon

---
+++ javascript.js
@@ -0,0 +1,11 @@
+
+
+function closeNav() {
+    nav = document.getElementById("nav");
+    nav.style.left = "100%";
+}
+
+function openNav() {
+    nav = document.getElementById("nav");
+    nav.style.left = "0%";
+}
