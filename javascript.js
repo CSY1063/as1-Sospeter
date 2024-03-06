1c87c22a204523a4ce2939c6dc556f14e08660dc
Sospeter <kedogosospeter36@gmail.com>
2024-03-06 21:59:23 +0000

    restored html files

---
+++ javascript.js
@@ -0,0 +1,11 @@
+
+
+function closeNav() {
+    nav = document.getElementById("nav");
+    nav.style.left = "-50em";
+}
+
+function openNav() {
+    nav = document.getElementById("nav");
+    nav.style.left = "0em";
+}
