71236a3f84e144131dee4213971814273947d00c
Sospeter <kedogosospeter36@gmail.com>
2024-03-07 00:30:36 +0000

    styled the contact form

--- javascript.js
+++ javascript.js
@@ -1,11 +1,29 @@
 
-
+/* moves the navigation off screen */
 function closeNav() {
     nav = document.getElementById("nav");
     nav.style.left = "-50em";
 }
 
+/* moves navigation on-screen */
 function openNav() {
     nav = document.getElementById("nav");
     nav.style.left = "0em";
+}
+
+let state = 0;
+
+/* Theme toggle  */ 
+function toggleTheme(){
+    toggle_btn = document.getElementById("toggleTheme");
+   
+    if (state == 0){
+        toggle_btn.innerHTML = "toggle_on"
+        state = 1;
+    } else {
+        toggle_btn.innerHTML = "toggle_off"
+        state = 0;
+    }
+
+    
 }
