d027e0765bab9fd3f6790acaa76134b4ee9894c0
Sospeter <kedogosospeter36@gmail.com>
2024-03-12 14:10:04 +0000

    inserted logo, styled contact page button

--- javascript.js
+++ javascript.js
@@ -11,19 +11,3 @@ function openNav() {
     nav.style.left = "0em";
 }
 
-let state = 0;
-
-/* Theme toggle  */ 
-function toggleTheme(){
-    toggle_btn = document.getElementById("toggleTheme");
-   
-    if (state == 0){
-        toggle_btn.innerHTML = "toggle_on"
-        state = 1;
-    } else {
-        toggle_btn.innerHTML = "toggle_off"
-        state = 0;
-    }
-
-    
-}
